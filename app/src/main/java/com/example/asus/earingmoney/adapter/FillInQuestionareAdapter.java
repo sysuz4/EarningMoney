a13efa14401214048c5a6f1f5f7dd8a47164ce19
zhangyzh36 <771235191@qq.com>
2019-06-24 23:11:44 +0800

    禁止填写已经填写过的问卷

--- app/src/main/java/com/example/asus/earingmoney/adapter/FillInQuestionareAdapter.java
+++ app/src/main/java/com/example/asus/earingmoney/adapter/FillInQuestionareAdapter.java
@@ -126,8 +126,11 @@ public class FillInQuestionareAdapter  extends BaseAdapter
 
     private void fillItem(int type, int i, View rootview) {
         if (type == 0) {
-            ((EditText)rootview).setText(list.get(i).getAnswer());
-            ((EditText)rootview).setId(i*100);
+            String answer = list.get(i).getAnswer();
+            if (answer != null && !answer.isEmpty()){
+                ((EditText) rootview).setText(answer);
+            }
+            ((EditText) rootview).setId(i * 100);
         } else if (type == 1) {
             List<String> options = Arrays.asList(list.get(i).getChoiceStr().split("\\|"));
             String option = list.get(i).getAnswer();
@@ -139,6 +142,10 @@ public class FillInQuestionareAdapter  extends BaseAdapter
                         .getLayoutParams();
                 layoutParams.setMargins(30, 5, 0, 5);//4个参数按顺序分别是左上右下
                 button.setLayoutParams(layoutParams);
+
+                if (option != null && !option.isEmpty()) {
+                    button.setEnabled(false);
+                }
             }
 
             if (option != null) {
@@ -148,6 +155,7 @@ public class FillInQuestionareAdapter  extends BaseAdapter
         } else {
             List<String> options = Arrays.asList(list.get(i).getChoiceStr().split("\\|"));
             String option = list.get(i).getAnswer();
+
             for (int j = 0; j < options.size(); ++j) {
                 CheckBox box = new CheckBox(context);
                 setCheckBoxAttribute(box, ch.substring(j, j+1) + "." + options.get(j), i*100 + j);
@@ -158,14 +166,23 @@ public class FillInQuestionareAdapter  extends BaseAdapter
                 box.setLayoutParams(layoutParams);
 
                 box.setOnCheckedChangeListener(this);
+
+                if (option != null && !option.isEmpty()) {
+                    box.setEnabled(false);
+                }
             }
             if (option != null) {
-                List<String> optionList = Arrays.asList(option.split(""));
-                for (int j = 0; j < optionList.size(); ++j) {
-                    int index = ch.indexOf(optionList.get(j));
+                //List<String> optionList = Arrays.asList(option.split(""));
+                //Toast.makeText(context, Integer.toString(optionList.size()), Toast.LENGTH_SHORT).show();
+                Log.e("option:", option);
+                for (int j = 0; j < option.length(); ++j) {
+                    int index = ch.indexOf(option.substring(j, j+1));
                     //int index = j;
                     CheckBox box = rootview.findViewById(i*100 + index);
                     box.setChecked(true);
+
+                    String answer = list.get(i).getAnswer();
+                    list.get(i).setAnswer(answer.substring(0, answer.length() - 1));
                 }
             }
         }
@@ -228,6 +245,8 @@ public class FillInQuestionareAdapter  extends BaseAdapter
             String newOption = deleteSubString(originOption, optionStr);
             list.get(questionIndex).setAnswer(newOption);
         }
+
+        Log.e("hh", list.get(questionIndex).getAnswer());
     }
 
     private String deleteSubString(String str1, String str2) {
